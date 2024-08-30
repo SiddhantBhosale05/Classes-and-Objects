82dbac95873017597995d3c69931bb1776231029
SiddhantBhosale05 <siddhant.bhosale05@gmail.com>
2024-08-30 11:55:21 +0530

    Add files via upload

---
+++ Defining Method Inside Class.cpp
@@ -0,0 +1,25 @@
+//Name:Siddhant Kiran Bhosale
+//PRN:23070123038
+//Aim:Implemention of Classes and Objects to Define Method inside Class
+
+#include<iostream>
+using namespace std;
+
+class Student               
+{
+    public:
+    void myMethod()
+    {
+        cout<<"I am studying in 2nd year";
+    }
+};
+
+int main()
+{
+    Student s1;
+    s1.myMethod();
+}
+
+
+/*Output:
+I am studying in 2nd year*/
