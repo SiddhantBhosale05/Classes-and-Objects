a928b178c4f2aed062fac3cf05c24fe46802d089
Siddhant Bhosale <siddhant.bhosale05@gmail.com>
2024-10-12 16:20:13 +0530

    Defining Method Inside Class.cpp

--- Defining Method Inside Class.cpp
+++ Defining Method Inside Class.cpp
@@ -1,6 +1,6 @@
 //Name:Siddhant Kiran Bhosale
 //PRN:23070123038
-//Aim:Implemention of Classes and Objects to Define Method inside Class
+//Aim:Implementation of Classes and Objects to Define Method inside Class
 
 #include<iostream>
 using namespace std;
